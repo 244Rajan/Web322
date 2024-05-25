1848829d189549ce3ac1a20bc93fdfb8d62f5ced
Rajan Yadav <144298714+244Rajan@users.noreply.github.com>
2024-05-25 00:09:23 -0400

    Update server.js
    included heading

--- my-app/server.js
+++ my-app/server.js
@@ -1,3 +1,17 @@
+/**
+ *  WEB322 - Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites, friends, GPT or otherwise) or distributed to other students.
+ *  I understand that if caught doing so, I will receive zero on this assignment and possibly 
+ *  fail the entire course.
+ *  Name: [Rajan Yadav]
+ *  Student ID: [157764226]
+ *  Date: [2024-05-25]
+ *  Vercel Web App URL: [https://vercel.com/rajan-yadavs-projects-30ed3093]
+ *  GitHub Repository URL: [https://github.com/244Rajan/Web322]
+ */
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
